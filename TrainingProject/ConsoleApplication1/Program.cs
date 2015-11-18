7b055b5353094f1d9703b76ef56cb4483bcbc563
dimashpin <dimashpin@outlook.com>
2015-11-18 18:32:38 +0200

    Added 2 new methods: SumOfNumbers and Reserse

---
+++ TrainingProject/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+
+    //Sum of numbers within a number
+    public static int SumOfNumbers (int a)
+    {
+        int result = 0;
+        //execute when true
+        while (a > 0)
+        {
+            result += a % 10;
+            a = a / 10;
+        }
+        return result;
+    }
+
+    //reverse order of numbers
+    public static int Reverse (int a)
+    {
+        int result = 0;
+        while (a > 0)
+        {
+            result *= 10;
+            result += a%10;
+            a = a / 10;
+        }
+        return result;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(SumOfNumbers(34234));
+        Console.WriteLine(Reverse(1234));
+    }
+}
---
+++ TrainingProject/TrainingProject/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Runtime.InteropServices; //used for the 4th method
+
+class Program
+{
+    //1. Using params keyword
+    public static void Sum1(int firstNumber, int secondNumber, params object[] otherNumbers)
+    {
+        int sum = firstNumber + secondNumber;
+        if (otherNumbers != null)
+        {
+            foreach (int n in otherNumbers)
+            {
+                sum += n;
+            }
+        }
+        Console.WriteLine("Sum1 = {0}", sum);
+    }
+
+    //2. Using overloading methods
+    public static void Sum2(int firstNumber, int secondNumber, int[] otherNumbers)
+    {
+        int sum = firstNumber + secondNumber;
+        if (otherNumbers != null)
+        {
+            foreach (int n in otherNumbers)
+            {
+                sum += n;
+            }
+        }
+        Console.WriteLine("Sum2 = {0}", sum);
+    }
+
+    public static void Sum2(int firstNumber, int secondNumber)
+    {
+        Sum2(firstNumber, secondNumber, null);
+    }
+
+    //3. Specifing default method's parameters values
+    public static void Sum3(int firstNumber, int secondNumber, int[] otherNumbers = null)
+    {
+        int sum = firstNumber + secondNumber;
+        if (otherNumbers != null)
+        {
+            foreach (int n in otherNumbers)
+            {
+                sum += n;
+            }
+        }
+        Console.WriteLine("Sum3 = {0}", sum);
+    }
+
+    //4. Using OptionalAttribute
+    public static void Sum4(int firstNumber, int secondNumber, [OptionalAttribute] int[] otherNumbers)
+    {
+        int sum = firstNumber + secondNumber;
+        if (otherNumbers != null)
+        {
+            foreach (int n in otherNumbers)
+            {
+                sum += n;
+            }
+        }
+        Console.WriteLine("Sum4 = {0}", sum);
+    }
+
+
+    static void Main()
+    {
+        //Testing
+        Sum1(23, 2);
+        Sum1(32, 22, 12);
+
+        Sum2(34, 2);
+        Sum2(2, 13, new int[] {11,9,2});
+
+        Sum3(4, 2);
+        Sum3(2, 13, new int[] { 11, 9});
+
+        Sum4(3, 2);
+        Sum4(2, 13, new int[] { 11, 52, 2 });
+    }
+}
+
